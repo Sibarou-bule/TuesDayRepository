a07ba8286486e875ec32ccccea0b98010032e8b1
Sibarou-bule <kingutanabe85@gmail.com>
2026-08-19 01:10:44 +0900

    課題のやつ

--- 2026 8 16/2026 8 18/Game.cpp
+++ 2026 8 16/2026 8 18/Game.cpp
@@ -7,12 +7,36 @@ using namespace std;
 
 void Game()
 {
+	int playerInput=0;
 	int ary[TOTAL_CARD];
+	int result[GROUP_CARD_1];
+	int k = 0;
+
+	bool endflag = false;
+	
 	CreateCard(ary);
 
 	ShuffleCard(ary);
 
+	for (int i = GROUP_CARD_2; i < GROUP_CARD_3; i++)
+	{
+		result[k] = ary[i];
+		k++;
+	}
+
 	ShowCard(ary);
+	ShowHiddenCard(ary);
+	PlayerCard(ary);
+	CPUCard(ary);
+
+	while (!endflag)
+	{
+		cout << "playerが当てるカードにある数字7枚を当てましょう\n";
+		InputChack(playerInput);
+
+		Result(result, playerInput, ary,endflag);
+	}
+	
 }
 
 void CreateCard(int cards[])
@@ -21,11 +45,11 @@ void CreateCard(int cards[])
 	int number = 0;
 	int count = 0;
 
-	for (number = 1; number <= MAX_NUMBER - 1; number++)
+	for (number = 1; number <= MAX_NUMBER; number++)
 	{
-		for (count = 0; count < number + 1; count++)
+		for (count = 0; count < number; count++)
 		{
-			cards[index] = number + 1;
+			cards[index] = number;
 			index++;
 		}
 	}
@@ -47,7 +71,8 @@ void ShuffleCard(int cards[])
 
 void ShowCard(int cards[])
 {
-	for (int i = 0; i < GROUP_CARD; i++)
+	cout << "オープンカード\n";
+	for (int i = 0; i < GROUP_CARD_1; i++)
 	{
 		cout << "[" << cards[i] << "]";
 	}
@@ -57,10 +82,90 @@ void ShowCard(int cards[])
 
 void ShowHiddenCard(int cards[])
 {
-	for (int i = 0; i < GROUP_CARD; i++)
+	cout << "クローズドカード\n";
+	for (int i = GROUP_CARD_1; i < GROUP_CARD_2; i++)
 	{
 		cout << "[?]";
 	}
 	cout << endl;
 }
 
+void PlayerCard(int cards[])
+{
+	cout << "playerが当てるカード\n";
+	for (int i = GROUP_CARD_2; i < GROUP_CARD_3; i++)
+	{
+		cout << "[?]";
+	}
+
+	cout << endl;
+}
+
+void CPUCard(int cards[])
+{
+	cout << "cpuのカード確認\n";
+	for (int i = GROUP_CARD_3; i < GROUP_CARD_4; i++)
+	{
+		cout << "[" << cards[i] << "]";
+	}
+
+	cout << endl;
+}
+
+void InputChack(int &num)
+{
+	while (true)
+	{
+		cout << "1～7の数字を入力してください\n";
+		cin >> num;
+		if (num < 1 || num>7)
+		{
+			cout << "再度入力してください\n";
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void Result(int cards[], int num, int card[], bool& flag)
+{
+	bool correct = false;
+	int remain = 0;
+
+	// 入力した数字があるか探す
+	for (int i = 0; i < GROUP_CARD_1; i++)
+	{
+		if (cards[i] == num)
+		{
+			cards[i] = -1;
+			correct = true;
+		}
+	}
+
+	if (correct)
+	{
+		cout << "正解！\n";
+	}
+	else
+	{
+		cout << "不正解！\n";
+	}
+
+	// まだ当てていないカードの枚数を数える
+	for (int i = 0; i < GROUP_CARD_1; i++)
+	{
+		if (cards[i] != -1)
+		{
+			remain++;
+		}
+	}
+
+	// 7枚すべて当てた
+	if (remain == 0)
+	{
+		cout << "7枚すべて正解しました！\n";
+		flag = true;
+	}
+}
--- 2026 8 16/2026 8 18/Game.h
+++ 2026 8 16/2026 8 18/Game.h
@@ -3,7 +3,13 @@
 //カードの総枚数
 const int TOTAL_CARD = 28;
 //1グループのカード枚数
-const int GROUP_CARD = 7;
+const int GROUP_CARD_1 = 7;
+//2グループのカード枚数
+const int GROUP_CARD_2 = 14;
+//3グループのカード枚数
+const int GROUP_CARD_3 = 21;
+//4グループのカード枚数
+const int GROUP_CARD_4 = 28;
 //カードの総枚数
 const int MAX_NUMBER = 7;
 
@@ -33,4 +39,18 @@ void ShowCard(int cards[]);
 /// <param name="cards"></param>
 void ShowHiddenCard(int cards[]);
 
+/// <summary>
+/// playerが見えないカード
+/// </summary>
+/// <param name="cards"></param>
+void PlayerCard(int cards[]);
 
+/// <summary>
+/// plyaerが見れるカード
+/// </summary>
+/// <param name="cards"></param>
+void CPUCard(int cards[]);
+
+void InputChack(int &num);
+
+void Result(int cards[], int num, int card[], bool &flag);
